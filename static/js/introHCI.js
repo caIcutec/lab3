32d899f3048ef276754dbd18cb7b3147c19189bf
caIcutec <jayang@ucsd.edu>
2020-01-23 10:54:00 -0800

    JQuery added

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,31 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("Button clicked!");
+		$('.jumbotron p').toggleClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$(".project").click(projectClick);
+}
+
+function projectClick(e){
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if (description.length==0){
+		containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	}else{
+		description.text("byeee!!!");
+		description.fadeOut();
+	}
 }
